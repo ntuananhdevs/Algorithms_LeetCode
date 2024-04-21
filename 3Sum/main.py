9ea709aea615ea0697dcf1a84c8682081e02230a
ntuananh_h <nguyentuananh.ndta@gmail.com>
2024-04-22 00:26:07 +0700

    hoan thien

--- 3Sum/main.py
+++ 3Sum/main.py
@@ -12,7 +12,24 @@ def threeSum(self, nums):
     N, P = set(n), set(p)    
 
     if z:
-        
+        for num in P:
+            if -1*num in N:
+                res.add((-1*num, 0, num))
+    if (len(z)) >= 3:
+        res.add((0, 0, 0))
+
+    for i in range(len(n)):
+        for j in range(i + 1, len(n)):
+            target = -1 * (n[i] + n[j])
+            if target in P:
+                res.add(tuple(sorted([n[i], n[j], target])))
+    for i in range(len(p)):
+        for j in range(i+1,len(p)):
+            target = -1 * (p[i] + p[j])
+            if target in N:
+                res.add(tuple(sorted([p[i], p[j],target])))
+    return res
+
     
 
             
