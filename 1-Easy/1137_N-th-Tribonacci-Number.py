6961dedab295e592c72c56aad75dea0bfcdd89c5
ntuananhdevs <anhndt25@gmail.com>
2024-05-28 12:16:01 +0700

    .

--- 1-Easy/1137_N-th-Tribonacci-Number.py
+++ 1-Easy/1137_N-th-Tribonacci-Number.py
@@ -24,14 +24,14 @@
 # 1. dung đệ quy(qua cham)
 
 
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1 or n == 2:
-            return 1
-        else:
-            return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         if n == 0:
+#             return 0
+#         elif n == 1 or n == 2:
+#             return 1
+#         else:
+#             return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
         
 
 #2.Dynamic Programming
